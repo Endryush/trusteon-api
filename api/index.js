18e48c6524295e4e8f3d32faa14b0dbee1f421fc
Endryus <endryush@gmail.com>
2024-10-08 19:37:23 -0300

    removing default port

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import server from './server.js'
 import './config/dotenv.js'
 
-const port = process.env.PORT || 5600
+const port = process.env.PORT
 
 server.listen(port, () => console.log(`API started on port ${port}`))
